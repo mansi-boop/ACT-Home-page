4f87e78d5a403276c372723051cddd0532a31a65
Mansi Tomar <85050610+mansi-boop@users.noreply.github.com>
2024-03-28 22:30:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:25,
+    nav:false,
+    center:true,
+
+   autoplay:true,
+
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1.5
+
+        }
+    }
+})
